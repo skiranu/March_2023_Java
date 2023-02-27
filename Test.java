ecd90a45bdbddf885b80713d754991502534a561
Surya <sury6195@gmail.com>
2023-02-26 19:47:37 -0800

    initilaized a square method object

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Surya!!!");
+        // initialized a square method object
         kiran k = new kiran();
         k.square(40);
     }
