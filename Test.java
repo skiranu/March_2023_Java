f2f4275446a923c1fd2ff65c62f776b4f2b5b1af
Surya <sury6195@gmail.com>
2023-02-26 19:44:20 -0800

    added a square method

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Surya!!!");
+<<<<<<< HEAD
         kiran k = new kiran();
         k.square(40);
     }
@@ -8,5 +9,8 @@ public class Test {
 class kiran{
     void square(int number){
         System.out.println("The square of the given number is: "+number*number);
+=======
+        System.out.println("Hello Surya YAYAYAYAYAYA!!!");
+>>>>>>> parent of d92ac66 (removed comment)
     }
 }
