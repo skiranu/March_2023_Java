cd6a62a8bbc151c9209e4fa25f0dcba37ebe23a6
Surya <sudayakumar@aromyx.com>
2023-02-26 18:10:49 -0800

    Abstract method program

---
+++ AbstractModVehicle.java
@@ -0,0 +1,29 @@
+abstract class AbstractModVehicle{
+    abstract public int getNumberOfWheels();
+    final public int getNumberOfSeats(){
+        return 2;
+    };
+    public static void main(String[] args){
+        Auto a = new Auto();
+        System.out.println(a.getNumberOfWheels());
+        Bus b = new Bus();
+        System.out.println(b.getNumberOfWheels());
+        System.out.println(b.getNumberOfSeats());
+    }
+}
+
+class Bus extends AbstractModVehicle{
+    public int getNumberOfWheels() {
+        return 7;
+    }
+    public int getNumberofSeats(){
+        return 222;
+    }
+}
+
+class Auto extends AbstractModVehicle{
+    public int getNumberOfWheels() {
+        return 3;
+    }
+}
+
